a12d2399db52a5d9171fc14324b63fd0f12529e0
tarun225601-star <tarun225601@gmail.com>
2026-09-11 11:07:35 +0530

    Fix syntax errors in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -894,9 +894,9 @@ class _VendorOrdersTabState extends State<VendorOrdersTab> {
     );
 
                        
-    );
-  }
-}
+      },
+);
+
 
 class VendorSettingsTab extends StatefulWidget {
   const VendorSettingsTab({super.key});
